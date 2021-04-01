85b5e0d88e9fbda15d18b2e83f53688622d38f74
Kevin Nguyen <knguy523@wch136-22.cs.ucr.edu>
2021-03-31 21:54:24 -0700

    Completed part 1

---
+++ turnin/knguy523_lab2_part1.c
@@ -0,0 +1,29 @@
+/*	Author: Kevin Nguyen  knguy523@ucr.edu
+ *  Partner(s) Name: 
+ *	Lab Section: 22
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: Garage open at night-- A garage door sensor connects to PA0 (1 means door open), and a light sensor connects to PA1 (1 means light is sensed). Write a program that illuminates an LED connected to PB0 (1 means illuminate) if the garage door is open at night.
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	unsigned tempA = 0x00;
+	unsigned tempB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	tempA = PINA & 0x03;
+	tempB = (tempA == 0x01) ? 0x01 : 0x00;
+	PORTB = tempB;
+    }
+    return 1;
+}
