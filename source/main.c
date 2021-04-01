429ee2d4acfd3a73d9ddb04713fd18235e2d4ea5
Kevin Nguyen <knguy523@wch136-22.cs.ucr.edu>
2021-03-31 21:10:37 -0700

    Work on part 1

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
-/*	Author: knguy523
+/*	Author: Kevin Nguyen  knguy523@ucr.edu
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 22
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: Garage open at night-- A garage door sensor connects to PA0 (1 means door open), and a light sensor connects to PA1 (1 means light is sensed). Write a program that illuminates an LED connected to PB0 (1 means illuminate) if the garage door is open at night.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -14,10 +14,18 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	unsigned tempA = 0x00;
+	unsigned tempB = 0x00;
     /* Insert your solution below */
     while (1) {
-
+	tempA = PINA & 0x02;
+	if(tempA == 0x01) {
+		tempB = 0x01;
+	}
+	PORTB = tempB;
     }
     return 1;
 }
